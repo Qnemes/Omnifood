1e51445e8e138a753801f4088516c6fbf53bfdb9
Qnemes <qnemes@gmail.com>
2020-05-08 11:48:24 +0200

    initial commit

---
+++ resources/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('.js-section-features').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: "80px;"
+    });
+});
